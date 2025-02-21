22181f015868053834498d8c64c5fcf20087c4bd
Chris Kucharczyk <chris.kucharczyk@gmail.com>
2025-02-21 16:18:34 +0000

    Add release workflow with data versioning (#29)
    * tie db name to version string
    
    * bump version for testing
    
    * add workflow with test release
    
    * update notebook
    
    * add prod PyPI to workflow
    
    * update notebook again
    
    * Update .github/workflows/release.yml [skip ci]
    
    Co-authored-by: Jay Qi <2721979+jayqi@users.noreply.github.com>
    
    * Update .github/workflows/release.yml [skip ci]
    
    Use tee instead of injecting directly into github environment
    
    Co-authored-by: Jay Qi <2721979+jayqi@users.noreply.github.com>
    
    * respond to feedback
    
    * add example version string [skip ci]
    
    * clean up names [skip ci]
    
    * remove weird looking typecast on version string and instead just noqa the offending line [skip ci]
    
    ---------
    
    Co-authored-by: Jay Qi <2721979+jayqi@users.noreply.github.com>

--- scipeds/__init__.py
+++ scipeds/__init__.py
@@ -1,1 +1,5 @@
+from importlib.metadata import version
+
+__version__ = version("scipeds")
+
 from scipeds.cli import download_db  # noqa:F401
--- scipeds/constants.py
+++ scipeds/constants.py
@@ -3,8 +3,11 @@ from pathlib import Path
 
 from platformdirs import user_cache_path
 
+from scipeds import __version__
+
 # DB attributes
-DB_NAME = "ipeds.duckdb"
+VERSION_STR = __version__.replace(".", "_")  # type: ignore[has-type]
+DB_NAME = f"scipeds_{VERSION_STR}.duckdb"
 COMPLETIONS_TABLE = "ipeds_completions_a"
 INSTITUTIONS_TABLE = "ipeds_directory_info"
 CIP_TABLE = "cip_info"
