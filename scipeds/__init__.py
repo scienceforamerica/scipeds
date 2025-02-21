f5dc2d132fc75480838c974a7074b9fe003b306d
Chris Kucharczyk <chris.kucharczyk@gmail.com>
2025-02-21 14:09:34 +0000

    remove weird looking typecast on version string and instead just noqa the offending line [skip ci]

--- scipeds/__init__.py
+++ scipeds/__init__.py
@@ -1,5 +1,5 @@
 from importlib.metadata import version
 
-__version__: str = version("scipeds")
+__version__ = version("scipeds")
 
 from scipeds.cli import download_db  # noqa:F401
--- scipeds/constants.py
+++ scipeds/constants.py
@@ -6,7 +6,7 @@ from platformdirs import user_cache_path
 from scipeds import __version__
 
 # DB attributes
-VERSION_STR = __version__.replace(".", "_")
+VERSION_STR = __version__.replace(".", "_")  # type: ignore[has-type]
 DB_NAME = f"scipeds_{VERSION_STR}.duckdb"
 COMPLETIONS_TABLE = "ipeds_completions_a"
 INSTITUTIONS_TABLE = "ipeds_directory_info"
